bd61f44c5fba6b318d577066cd1e1c1eb00d43cf
Muhammad Umer <mhadisb1212@gmail.com>
2024-03-13 11:38:36 +0500

    stateful-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,7 +11,45 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter App',
       themeMode: ThemeMode.system,
-      home: Home(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stateful Widget tut'),
+          centerTitle: true,
+        ),
+        body: const SFWidget(),
+      ),
+    );
+  }
+}
+
+class SFWidget extends StatefulWidget {
+  const SFWidget({super.key});
+
+  @override
+  State<SFWidget> createState() => _SFWidgetState();
+}
+
+class _SFWidgetState extends State<SFWidget> {
+  bool liked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text("Adidas"),
+          trailing: IconButton(
+            icon: liked
+                ? (const Icon(Icons.favorite))
+                : (const Icon(Icons.favorite_border)),
+            onPressed: () {
+              setState(() {
+                liked = !liked;
+              });
+            },
+          ),
+        )
+      ],
     );
   }
 }
